15b7f6bc5a5e6f90e9815819a8f85dc065a9ad97
shenyuanyuan10 <shenyuanyuan@jd.com>
2018-12-14 18:31:17 +0800

    添加对应代码

--- app.js
+++ app.js
@@ -10,56 +10,32 @@ var app = express()
 
 app.engine('art', require('express-art-template'));
 app.set('view options', {
-    debug: process.env.NODE_ENV !== 'production'
+  debug: process.env.NODE_ENV !== 'production'
 });
 
-
-app.get('/index', function(req, res, next) {
-    res.render('index.art', {
-        user: {
-            name: 'aui',
-            tags: ['art', 'template', 'nodejs']
-        }
-    });
-    next()
+// 使用art-template例子
+app.get('/index', function (req, res, next) {
+  res.render('index.art', {
+    user: {
+      name: 'art-template',
+      tags: ['art', 'template', 'nodejs']
+    }
+  });
 });
 
-var myLogger = function(req, res, next) {
-    console.log('LOGGED')
-    next()
-    console.log('After LOGGED')
-}
-
-var myLogger2 = function(req, res, next) {
-    console.log('LOGGED2')
-    next();
-    console.log('After LOGGED2')
-}
-
-app.use(myLogger)
-app.use(myLogger2)
-
-
-
-
-app.get('/', function(req, res) {
-    request('http://api.douban.com/v2/movie/in_theaters', function(error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        res.send(body)
-    });
+// 获取豆瓣接口例子
+app.get('/', function (req, res) {
+  request('http://api.douban.com/v2/movie/in_theaters', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send(body)
+  });
 
 })
-
-app.get('/about', function(req, res) {
-    res.send('hello about ...')
-})
-
-// app.use(express.static('./public/'))
 
 app.use('/public/', express.static('./public/'))
 
-app.listen(3000, function() {
-    console.log('express app is runing .....')
+app.listen(3002, function () {
+  console.log('express app is runing .....')
 })
